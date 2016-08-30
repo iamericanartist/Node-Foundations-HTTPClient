aa1e72a3f949fc9f7a35f93b84e8bbbfa59762a7
Eric Denton <eric@iamericanartist.com>
2016-08-29 23:49:37 -0500

    progress

--- 11.js
+++ 11.js
@@ -4,8 +4,30 @@
 
 //////////////////////////// EXECUTE with "./11.js" ////////////////////////////
 ////// ran "chmod +x 11.js" from TERMINAL to allow execution capabilities //////
+///////////////////////  npm install request (for "GET") ///////////////////////
+///////////////////////  npm install cheerio (for "LOAD") //////////////////////
 
+const { createServer } = require('http')
+const { get } = require('request')
+const { load } = require('cheerio')
 
+const server = createServer()
 
+server.on('request', (req, res) => {
+  get('https://reddit.com', (err, _, body) => {
 
-/////////////////////////////////// LEARNING ///////////////////////////////////
+    const $ = load(body)
+
+    const topStories = $('a.title')
+      .toArray()
+      .map($)                     // .map((el) => $(el))
+      .map(el => ({
+        title: el.text(),
+        link: el.attr('href')
+      }))
+
+    res.end(JSON.stringify(topStories))
+  })
+})
+
+server.listen(8080)
